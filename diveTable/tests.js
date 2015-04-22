a9e517e7ba966c56be9448b1a3f9d97c94f5e51a
Justin Watt <justinwatt@gmail.com>
2015-04-22 09:53:03 -0700

    Add surface interval and dive

--- diveTable/tests.js
+++ diveTable/tests.js
@@ -1,3 +1,19 @@
+/* Dive Steps
+
+   // Dive Table
+   1. Pick a depth to dive to
+   2. Pick amount of time to stay at that depth
+   3. Calculate current dive group
+
+   // Surface Interval Table
+   4. Pick duration of Surface Interval
+   5. Calculate new dive group after Surface Interval
+
+   // Repetitive Dive Table
+   6. Calculate Residual Nitrogen Time
+
+*/
+
 QUnit.module("Naui Table Tests", {
   beforeEach: function(){
     this.d = new NauiDiveTable();
@@ -23,21 +39,13 @@ QUnit.test('timeColumnLookUp', function(assert){
   assert.strictEqual(this.d.timeColumnLookUp(2, 45, 0), 7);
 });
 
-/* Dive Steps
+QUnit.test('SurfaceGroupLookUp', function(assert){
+  assert.strictEqual(this.d.surfaceTableLookUp(3, 30), 3, 'Same Group');
+  assert.strictEqual(this.d.surfaceTableLookUp(3, 10), 3, 'Minimum Rest');
+  assert.strictEqual(this.d.surfaceTableLookUp(2, 100), 1, 'Move up one group');
+  assert.strictEqual(this.d.surfaceTableLookUp(3, 360), 0, 'Move up two groups');
+});
 
-   // Dive Table
-   1. Pick a depth to dive to
-   2. Pick amount of time to stay at that depth
-   3. Calculate current dive group
-
-   // Surface Interval Table
-   4. Pick duration of Surface Interval
-   5. Calculate new dive group after Surface Interval
-
-   // Repetitive Dive Table
-   6. Calculate Residual Nitrogen Time
-
-*/
 
 QUnit.module("Diver Tests", {
   beforeEach: function(){
@@ -46,7 +54,22 @@ QUnit.module("Diver Tests", {
   }
 });
 
-QUnit.test('', function(assert) {
+QUnit.test('Dive Test', function(assert) {
   this.diver.dive(50, 25);
   assert.ok( this.diver.currentGroup === "D", "Correct Group!" );
 });
+
+QUnit.test('SIT Test', function(assert) {
+  this.diver.dive(50, 25);
+  this.diver.surface(3, 30);
+  assert.ok( this.diver.currentGroup === "D", "Correct Group!" );
+});
+
+
+// Based on example from http://www.scubadiverinfo.com/2_divetables.html
+QUnit.test('Dive and Surface', function(assert) {
+  this.diver.dive(35, 60);
+  this.diver.surface(this.diver.currentGroupIndex, 30);
+  assert.ok( this.diver.currentGroup === "G", "Correct Group!" );
+});
+
